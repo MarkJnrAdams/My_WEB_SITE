84c03540ae090ea7094762370a38472dd89c7fc5
MarkJnrAdams <54513106+MarkJnrAdams@users.noreply.github.com>
2019-08-27 14:40:04 +0100

    Add files via upload

---
+++ App.java
@@ -0,0 +1,30 @@
+import java.util;
+public class App{
+    public static void main (String []args){
+    int num1, num2, num3,sum, ave;
+    Scanner myscanner = new Scanner (System.in);
+    System.out.println("enter the first number ");
+    num1 = myscanner.nextDouble();
+    System.out.println("enter the second number ");
+    num2 = myscanner.nextDouble();
+    System.out.println("enter the th number ");
+    System.out.println("enter the th number ");
+    num3 = myscanner.nextDouble();
+    sum = num1 + num2 + num3;
+    ave = sum/3;
+    System.out.println();
+
+
+
+
+
+    }
+
+
+
+
+
+    
+
+
+}
